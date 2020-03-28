df0c2b950c3c34d233e84eac100d4acffd1b2bdf
Tre' Seymour <wayne.seymour@elastic.co>
2020-03-28 15:57:18 -0600

    Reverse the tree...if LEAF, just return.

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ class Node extends Tree {
   matchWith(pattern) {
     return pattern.Node(this.left, this.v, this.right);
   }
+
+  toString() {
+    return `Node(${this.left.toString()},${(this.v)},${this.right.toString()})`;
+  }
 }
 
 class Leaf extends Tree {
@@ -19,11 +23,27 @@ class Leaf extends Tree {
     super()
     this.v = v;
   }
-  matchWith( pattern) {
-    return  pattern.Leaf(this.v);
+
+  matchWith(pattern) {
+    return pattern.Leaf(this.v);
+  }
+
+  toString() {
+    return (`Leaf(${this.v} )`);
   }
 }
 
-firstTree = new Node(
-  new Node(new Leaf(7), 3, new Leaf(3)), 6, new Node(new Leaf(8), 4, new Leaf(1))
-);
+
+Tree.prototype.reverse = function () {
+  return this.matchWith({
+    Leaf: v => new Leaf(v),
+    Node: (left, v, right) => new Node(right.reverse(), v, left.reverse())
+  });
+}
+const treeInstance =
+  new Node(new Node(new Leaf(2), 4, new Leaf(2)), 6, new Node(new Leaf(7), 8, new Leaf(9)));
+
+console.log(treeInstance.toString());
+
+const reverse = treeInstance.reverse();
+console.log(reverse.toString())
